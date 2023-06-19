69abb2c44f240edb79cdbd6aeb4dcbc533861e8c
Yevhenii Sukhominskiy <yevhenii.sukhminskiy@gmail.com>
2023-06-19 11:57:28 +0300

    Added the Dryer class

--- src/main.cpp
+++ src/main.cpp
@@ -28,6 +28,60 @@
 #define TEMP_QUIZ 5000          // Час опитування датчика
 /*------------------------------------------------------------*/
 
+class Dryer
+{
+private:
+    /*-----------------НАЛАШТУВАННЯ ОБ'ЄКТІВ КНОПОК-----------------*/  
+    EncButton<EB_TICK, 2> buttonTempUp;     // Температура додавання    
+    EncButton<EB_TICK, 3> buttonTempDown;   // Температура віднімання 
+    EncButton<EB_TICK, 4> buttonTimerUp;    // Таймер додавання 
+    EncButton<EB_TICK, 5> buttonTimerDown;  // Таймер віднімання
+    EncButton<EB_TICK, 6> buttonMode;       // Вибір режиму
+    EncButton<EB_TICK, 7> buttonStart;      // Старт/стоп/блокування
+    /*--------------------------------------------------------------*/
+
+    /*-----------------НАЛАШТУВАННЯ ОБ'ЄКТІВ ДИСПЛЕЇВ---------------*/
+    GyverTM1637 disp_ds;            // Дисплей температури	
+    GyverTM1637 disp_ta;            // Дисплей таймеру
+    /*--------------------------------------------------------------*/
+
+    /*-----------НАЛАШТУВАННЯ ОБ'ЄКТІВ ДАТЧИКА ТЕМПЕРАТУРИ----------*/
+    OneWire oneWire;            // Ініціалізація об'єкта протокола
+    DallasTemperature sensors;  // Ініціалізація об'єкта температури
+    /*--------------------------------------------------------------*/
+
+    /*----------------------ЗБЕРІГАННЯ ЗМІННИХ----------------------*/
+    float temperature;  // Температура
+    byte hours;         // Години
+    byte minutes;       // Хвилини
+    bool timerRunning;  // Статус таймера
+    float tempC;        // Зберігння температури для виводу
+    bool celsiusSign;   // Статус зміни режиму на дисплею температури
+    /*--------------------------------------------------------------*/
+
+public:
+Dryer() :
+        buttonTempUp(),
+        buttonTempDown(),
+        buttonTimerUp(),
+        buttonTimerDown(),
+        buttonMode(),
+        buttonStart(),
+        disp_ds(CLK_DS, DIO_DS),
+        disp_ta(CLK_TA, DIO_TA),
+        oneWire(DS18B20),
+        sensors(&oneWire),
+        temperature(TEMP_MIN),
+        hours(0),
+        minutes(0),
+        timerRunning(false),
+        tempC(),
+        celsiusSign(false)
+    {}
+};
+
+Dryer dryer;
+
 void setup() 
 {
 
